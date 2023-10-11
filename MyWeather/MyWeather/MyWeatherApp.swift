04700bb71bb7a5c24dc2de794198ecfc927967c9
VD <vd.com>
2023-10-11 16:26:21 +0300

    Added PreloaderView

---
+++ MyWeather/MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Valerii on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PreloaderView()
+        }
+    }
+}
---
+++ MyWeather/MyWeather/View and ViewModel/Preloader/PreloaderView.swift
@@ -0,0 +1,72 @@
+//
+//  PreloaderView.swift
+//  MyWeather
+//
+//  Created by Valerii on 08.10.2023.
+//
+
+import SwiftUI
+
+struct PreloaderView: View {
+
+//MARK: - Body
+    
+    var body: some View {
+        ZStack {
+            // BACKGROUND
+            Color.backgroundColor
+               .ignoresSafeArea()
+
+            // MAIN CONTENT
+            VStack(spacing: 20) {
+                // IMAGE
+                Image(ViewImage.sunIcon.image)
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                
+                Text(ViewText.title.text)
+                    .font(.system(size: 70, weight: .bold))
+                    .foregroundColor(.selectColor)
+            }
+        }
+    }
+}
+
+//MARK: - Preview
+
+#Preview {
+    PreloaderView()
+}
+
+//MARK: - Extension
+
+extension PreloaderView {
+    //MARK: VIEW COMPONENTS
+    
+    // HEADER
+    private var header: some View {
+        Text("Hello")
+    }
+}
+
+//MARK: - Resources
+
+extension PreloaderView {
+    // text
+    enum ViewText: String {
+        case title = "Weather"
+
+    var text: String {
+            rawValue
+        }
+    }
+    
+    // image name
+    enum ViewImage: String {
+        case sunIcon
+
+    var image: String {
+            rawValue
+        }
+    }
+}
